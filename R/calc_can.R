ea47a51aa054e58fb925db81a57c2b294fcfbc00
icrichmond <icrichmond@mun.ca>
2023-05-25 11:28:43 -0400

    add geometry to cancov

--- R/calc_can.R
+++ R/calc_can.R
@@ -6,22 +6,21 @@ calc_can <- function(rv, canopy){
     filter(CODE_ARR == "VSMPE") %>%
     # select relevant columns
     select(RUELLE_ID, CODE_ARR, geometry) %>%
+    # make 10 m buffer to ensure edges of ruelles are included
+    st_buffer(10) %>%
     # combine ruelles with the same ID
     group_by(RUELLE_ID) %>%
     summarize(geometry = st_union(geometry)) %>%
     mutate(ruelle_area = st_area(geometry))
   
-  # make a buffer of 10 m to make sure edges of ruelles are included 
-  rv_b <- st_buffer(rv_t, 10)
-  
   # intersect canopy with ruelles
-  can_i <- canopy[rv_b, ]
+  can_i <- canopy[rv_t, ]
   
   # convert into sf object
   can_sf <- st_as_sf(can_i)
   
   # associate with ruelles
-  can_a <- st_intersection(can_sf, rv_b)
+  can_a <- st_intersection(can_sf, rv_t)
   
   # union pixels for each ruelle, calculate their area, and then % canopy
   can_cov <- can_a %>% 
@@ -33,9 +32,12 @@ calc_can <- function(rv, canopy){
     st_drop_geometry()
   
   # associate with ruelle geometry
-  can_rv <- inner_join(can_cov, rv_b, by = "RUELLE_ID")
+  can_rv <- inner_join(can_cov, rv_t, by = "RUELLE_ID") %>%
+    rename(ruelle_area = ruelle_area.x) %>%
+    select(-ruelle_area.y) %>%
+    st_as_sf()
   
 
-  return(can_cov)
+  return(can_rv)
   
 }
