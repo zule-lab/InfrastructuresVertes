3e7dbf1f2282dc4cd812589b7ad0cd8c02fcf973
icrichmond <icrichmond@mun.ca>
2023-05-25 11:05:06 -0400

    can cov with INSQ data

--- R/calc_can.R
+++ R/calc_can.R
@@ -8,12 +8,34 @@ calc_can <- function(rv, canopy){
     select(RUELLE_ID, CODE_ARR, geometry) %>%
     # combine ruelles with the same ID
     group_by(RUELLE_ID) %>%
-    summarize(geometry = st_union(geometry))
+    summarize(geometry = st_union(geometry)) %>%
+    mutate(ruelle_area = st_area(geometry))
   
   # make a buffer of 10 m to make sure edges of ruelles are included 
   rv_b <- st_buffer(rv_t, 10)
+  
+  # intersect canopy with ruelles
+  can_i <- canopy[rv_b, ]
+  
+  # convert into sf object
+  can_sf <- st_as_sf(can_i)
+  
+  # associate with ruelles
+  can_a <- st_intersection(can_sf, rv_b)
+  
+  # union pixels for each ruelle, calculate their area, and then % canopy
+  can_cov <- can_a %>% 
+    group_by(RUELLE_ID) %>% 
+    summarize(geometry = st_union(geometry), 
+              ruelle_area = first(ruelle_area), 
+              canopy_area = st_area(geometry),
+              per_can = canopy_area/ruelle_area) %>%
+    st_drop_geometry()
+  
+  # associate with ruelle geometry
+  can_rv <- inner_join(can_cov, rv_b, by = "RUELLE_ID")
+  
 
-    
-  return(rv_can)
+  return(can_cov)
   
 }
