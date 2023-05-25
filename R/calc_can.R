327fedc7b232507932c67a60a8f88235728885f6
icrichmond <icrichmond@mun.ca>
2023-05-25 08:42:30 -0400

    union ruelles with same id then calc can cov

--- R/calc_can.R
+++ R/calc_can.R
@@ -2,13 +2,19 @@ calc_can <- function(rv, canopy){
   
   # match projections
   rv_t <- rv %>% st_transform(st_crs(canopy)) %>%
-    # create 10 m buffer around ruelles
-    st_buffer(dist = 10) %>%
-    # select Villeray-Saint Michel-Parc Extension
-    filter(CODE_ARR == "VSMPE") 
+    # filter for the neighbourhood
+    filter(CODE_ARR == "VSMPE") %>%
+    # select relevant columns
+    select(RUELLE_ID, CODE_ARR, geometry) %>%
+    # combine ruelles with the same ID
+    group_by(RUELLE_ID) %>%
+    summarize(geometry = st_union(geometry))
+  
+  # make a buffer of 10 m to make sure edges of ruelles are included 
+  rv_b <- st_buffer(rv_t, 10)
   
   # calculate canopy cover by dividing number of pixels == 4 (canopy) by total number of pixels within a buffer
-  rv_can <- aggregate(canopy, rv_t, FUN = function(x) sum(x == 4)/length(x)) %>% 
+  rv_can <- aggregate(canopy, rv_b, FUN = function(x) sum(x == 4)/length(x)) %>% 
     st_as_sf() %>%
     rename(percan_2021 = `66023_IndiceCanopee_2021.tif`) %>%
     mutate(percan_2021 = round(percan_2021, 3),
