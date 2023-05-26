5e88eda432830cc496b59768f8419cbaae478dd7
icrichmond <icrichmond@mun.ca>
2023-05-26 12:37:08 -0400

    can no longer use sf approach - too large of vector

--- R/calc_can.R
+++ R/calc_can.R
@@ -9,26 +9,26 @@ calc_can <- function(rv, canopy){
   # intersect canopy with ruelles
   can_i <- canopy[rv_t, ]
   
-  # convert into sf object
-  can_sf <- st_as_sf(can_i)
-  
-  # associate with ruelles
-  can_a <- st_intersection(can_sf, rv_t)
-  
-  # union pixels for each ruelle, calculate their area, and then % canopy
-  can_cov <- can_a %>% 
-    group_by(RUELLE_ID) %>% 
-    summarize(geometry = st_union(geometry), 
-              ruelle_area = first(ruelle_area), 
-              canopy_area = st_area(geometry),
-              per_can = canopy_area/ruelle_area) %>%
-    st_drop_geometry()
-  
-  # associate with ruelle geometry
-  can_rv <- inner_join(can_cov, rv_t, by = "RUELLE_ID") %>%
-    rename(ruelle_area = ruelle_area.x) %>%
-    select(-ruelle_area.y) %>%
-    st_as_sf()
+#  # convert into sf object
+#  can_sf <- st_as_sf(can_i)
+#  
+#  # associate with ruelles
+#  can_a <- st_intersection(can_sf, rv_t)
+#  
+#  # union pixels for each ruelle, calculate their area, and then % canopy
+#  can_cov <- can_a %>% 
+#    group_by(RUELLE_ID) %>% 
+#    summarize(geometry = st_union(geometry), 
+#              ruelle_area = first(ruelle_area), 
+#              canopy_area = st_area(geometry),
+#              per_can = canopy_area/ruelle_area) %>%
+#    st_drop_geometry()
+#  
+#  # associate with ruelle geometry
+#  can_rv <- inner_join(can_cov, rv_t, by = "RUELLE_ID") %>%
+#    rename(ruelle_area = ruelle_area.x) %>%
+#    select(-ruelle_area.y) %>%
+#    st_as_sf()
   
 
   return(can_rv)
