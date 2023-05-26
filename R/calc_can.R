700c66e6ab648702c98f3a2e1cee647878fc3f0b
icrichmond <icrichmond@mun.ca>
2023-05-26 13:00:46 -0400

    aggregate for can cov

--- R/calc_can.R
+++ R/calc_can.R
@@ -9,26 +9,15 @@ calc_can <- function(rv, canopy){
   # intersect canopy with ruelles
   can_i <- canopy[rv_t, ]
   
-#  # convert into sf object
-#  can_sf <- st_as_sf(can_i)
-#  
-#  # associate with ruelles
-#  can_a <- st_intersection(can_sf, rv_t)
-#  
-#  # union pixels for each ruelle, calculate their area, and then % canopy
-#  can_cov <- can_a %>% 
-#    group_by(RUELLE_ID) %>% 
-#    summarize(geometry = st_union(geometry), 
-#              ruelle_area = first(ruelle_area), 
-#              canopy_area = st_area(geometry),
-#              per_can = canopy_area/ruelle_area) %>%
-#    st_drop_geometry()
-#  
-#  # associate with ruelle geometry
-#  can_rv <- inner_join(can_cov, rv_t, by = "RUELLE_ID") %>%
-#    rename(ruelle_area = ruelle_area.x) %>%
-#    select(-ruelle_area.y) %>%
-#    st_as_sf()
+  can_a <- aggregate(can_i, rv_t, FUN = function(x){sum(!is.na(x))}) %>% 
+    st_as_sf() %>%
+    # number of pixels = area because pixels are 1m2
+    rename(canopy_area = `file4eb83feb10d8.tif`) %>%
+    mutate(RUELLE_ID = rv_t$RUELLE_ID) %>%
+    st_drop_geometry()
+  
+  can_rv <- inner_join(can_a, rv_t, by = "RUELLE_ID") %>%
+    mutate(per_can = drop_units(canopy_area/ruelle_area))
   
 
   return(can_rv)
