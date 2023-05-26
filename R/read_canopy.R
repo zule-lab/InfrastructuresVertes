02727d6affe9503db5c2005548e09a7d04776ebb
Alec L. Robitaille <robit.alec@gmail.com>
2023-05-26 14:39:10 -0300

    save canopy mosaic to non-temp file

--- R/read_canopy.R
+++ R/read_canopy.R
@@ -4,7 +4,7 @@ read_canopy <- function(mtl_can, tr_can) {
   
   tr <- read_stars(tr_can)
   
-  can <- st_mosaic(mtl, tr)
+  can <- st_mosaic(mtl, tr, dst = file.path('results', 'canopy_mosaic'))
   
   return(can$canopy_mosaic)
   
