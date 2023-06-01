3f4212a43899e22b677495a0f3919e920055e8e5
icrichmond <icrichmond@mun.ca>
2023-06-01 14:32:36 -0400

    add ruelle area and % can, remove old code

--- R/calc_can.R
+++ R/calc_can.R
@@ -1,38 +1,28 @@
 calc_can <- function(rv, canopy_path){
   
+  # ruelle area
+  rv_a <- rv %>%
+    summarize(RUELLE_ID = RUELLE_ID, 
+              CODE_ARR = CODE_ARR, 
+              geometry = st_union(geometry)) %>%
+    mutate(ruelle_area = st_area(geometry))
+  
+  
+  # canopy area 
   canopy <- read_stars(canopy_path)
   
   # [[1]] to grab the value returned (instead of sf object) to save in 
   #  as column
-  rv$canopy <- aggregate(
+  rv_a$canopy <- aggregate(
     canopy,
-    rv, 
+    rv_a, 
     FUN = function(x) sum(!is.na(x))
   )[[1]]
   
-  return(rv)
   
-  # rv_t <- rv %>%
-  #   # combine ruelles with the same ID
-  #   group_by(RUELLE_ID) %>%
-  #   summarize(geometry = st_union(geometry)) %>%
-  #   mutate(ruelle_area = st_area(geometry))
-  # 
-  # # intersect canopy with ruelles
-  # can_i <- canopy[rv_t, ]
-  # 
-  # can_a <- aggregate(can_i, rv_t, FUN = function(x){sum(!is.na(x))}) %>% 
-  #   st_as_sf() %>%
-  #   # number of pixels = area because pixels are 1m2
-  #   rename(canopy_area = `file4eb83feb10d8.tif`) %>%
-  #   mutate(RUELLE_ID = rv_t$RUELLE_ID) %>%
-  #   st_drop_geometry()
-  # 
-  # can_rv <- inner_join(can_a, rv_t, by = "RUELLE_ID") %>%
-  #   mutate(per_can = drop_units(canopy_area/ruelle_area)) %>%
-  #   st_as_sf()
-  # 
-  # 
-  # return(can_rv)
+  # percent canopy cover
+  rv_a$per_can <- drop_units(rv_a$canopy/rv_a$ruelle_area)
+  
+  return(rv_a)
   
 }
