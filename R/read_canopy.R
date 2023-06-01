6aa9a6ffc0f99b40fc001d969e34b5640f092569
icrichmond <icrichmond@mun.ca>
2023-06-01 14:32:44 -0400

    add file ext

--- R/read_canopy.R
+++ R/read_canopy.R
@@ -5,7 +5,7 @@ read_canopy <- function(mtl_can, tr_can) {
   tr <- read_stars(tr_can)
   
   # Save mosaic as file in results/
-  can <- st_mosaic(mtl, tr, dst = file.path('results', 'canopy_mosaic'))
+  can <- st_mosaic(mtl, tr, dst = file.path('results', 'canopy_mosaic.tif'))
   
   # Return path of output mosaic
   return(can$canopy_mosaic)
