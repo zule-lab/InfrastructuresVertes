8e803b86c8d71bda9ddc5418eb1e320e9045da93
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-02-05 15:49:14 -0500

    montreal map rough

--- R/create_figure_1.R
+++ R/create_figure_1.R
@@ -1,6 +1,6 @@
 create_figure_1 <- function(study_rv, study_controls, insects, quartiers){
   
-  rds <- shp <- st_read(file.path("/vsizip", 'input/roads.zip')) %>% 
+  rds <- st_read(file.path("/vsizip", 'input/roads.zip')) %>% 
     filter(CATEGORIEC == 'Autoroute')
   
 # Data separation ---------------------------------------------------------
@@ -16,11 +16,14 @@ create_figure_1 <- function(study_rv, study_controls, insects, quartiers){
   
   insects$insects <- 'yes'
 
-  insects <- left_join(mont, st_set_geometry(insects, NULL)) 
+  insects <- left_join(mont, st_set_geometry(insects, NULL))  %>% 
+    mutate(insects = replace(insects, is.na(insects), 'no'))
   
   insects_pts <- insects %>% 
-    replace(is.na(insects), 'no') %>% 
     st_centroid()
+  
+  quartiers <- quartiers %>% 
+    mutate(Q_socio = replace(Q_socio, Q_socio == 'Parc-Extension', 'Parc-Ex'))
     
   tr_rv <- study_rv %>% 
     filter(CODE_ARR == "TR")
@@ -28,43 +31,70 @@ create_figure_1 <- function(study_rv, study_controls, insects, quartiers){
     filter(CODE_ARR == "TR")
   tr <- rbind(tr_rv, tr_con)
   
-# Montreal context --------------------------------------------------------
-
-
-# 3R context  -------------------------------------------------------------
-
-  
   
 
 # Montreal map ------------------------------------------------------------
   
   bb <- st_bbox(st_buffer(mont, 500))
   
-  # canopy cover?
-  # insects 
-  
-  ggplot() + 
-    geom_sf(data = insects_pts, aes(size = per_can), colour = "darkgrey", alpha = 0.75) +
+  main <- ggplot() + 
+    geom_sf(data = insects_pts, aes(size = per_can), colour = "black", shape = 1) +
     geom_sf(data = mont, aes(fill = group, colour = group), linewidth = 0.8) +  
     geom_sf(data = quartiers, fill = NA, colour = "black", linetype = 'dashed', linewidth = 0.5) +
     geom_sf(data = rds, colour = "black", fill = "black") + 
-    scale_colour_manual(values = c("white", "darkgreen")) +
-    scale_fill_manual(values = c("white", "darkgreen")) + 
+    scale_colour_manual(values = c("darkgrey", "darkgreen")) +
+    scale_fill_manual(values = c("darkgrey", "darkgreen")) + 
     geom_text_repel(data = quartiers, aes(label = Q_socio, geometry = geometry), stat = "sf_coordinates") + 
     coord_sf(xlim = bb[c(1, 3)], ylim = bb[c(2, 4)]) +
-    #scale_colour_manual(values = c("white", "darkgreen")) + 
     labs(fill = "", colour = "", size = "Percent Canopy") + 
     theme(panel.border = element_rect(linewidth = 1, fill = NA),
-          panel.background = element_rect(fill = '#ddc48d'),
+          panel.background = element_rect(fill = '#f3e3bf'),
           panel.grid = element_blank(),
           axis.text = element_text(size = 11, color = 'black'),
           axis.title = element_blank(),
           plot.background = element_rect(fill = NA, colour = NA),
           legend.position = 'top')
   
+  inset <- ggplot() + 
+    geom_sf(data = insects %>% filter(insects != 'no'), colour = 'goldenrod3', fill = 'goldenrod3', linewidth = 0.8) +
+    geom_sf(data = quartiers, fill = NA, colour = "black", linetype = 'dashed', linewidth = 0.5) +
+    geom_sf(data = rds, colour = "black", fill = "black") + 
+    coord_sf(xlim = bb[c(1, 3)], ylim = bb[c(2, 4)]) +
+    guides(fill = "none", colour = "none") +  
+    theme(panel.border = element_rect(linewidth = 1, fill = NA),
+          panel.background = element_rect(fill = 'white'),
+          panel.grid = element_blank(),
+          axis.text = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(), 
+          plot.background = element_rect(fill = NA, colour = NA),
+          legend.position = 'top')
+  
+  ggdraw(main) +
+    draw_plot(
+      {
+        inset
+      },
+      # The distance along a (0,1) x-axis to draw the left edge of the plot
+      x = 0.5, 
+      # The distance along a (0,1) y-axis to draw the bottom edge of the plot
+      y = 0.05,
+      # The width and height of the plot expressed as proportion of the entire ggdraw object
+      width = 0.30, 
+      height = 0.30)
+
+    
 
 # 3R map ------------------------------------------------------------------
-
+  
+  bbtr <- st_bbox(st_buffer(tr, 500))
+  
+  
+  
+  
+  
+  ggplot() + 
+    geom_sf(data = tr)
 
 
   
