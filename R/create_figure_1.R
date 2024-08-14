0e403edfda9286412f435fba1c2dd839a6a3625f
icrichmond <bellarichmond@proton.me>
2024-08-14 11:15:33 -0400

    move insect ruelle selection inside fig 1

--- R/create_figure_1.R
+++ R/create_figure_1.R
@@ -1,9 +1,42 @@
-create_figure_1 <- function(study_rv, study_controls, insects, quartiers){
+create_figure_1 <- function(study_rv, study_controls, quartiers){
   
   rds <- st_read(file.path("/vsizip", 'input/roads.zip')) %>% 
     filter(CATEGORIEC == 'Autoroute')
   
   quartiers_tr <- read_sf('input/Quartiers_3R/Quartier_3R.shp')
+  
+  # Insect ruelles ----------------------------------------------------------
+  
+  all <- rbind(study_rv, study_controls)
+  
+  insects <- all %>%
+    filter(RUELLE_CODE == "RV-VSMPE-2" | 
+             RUELLE_CODE == "RV-VSMPE-3" | 
+             RUELLE_CODE == "RV-VSMPE-6" | 
+             RUELLE_CODE == "RV-VSMPE-8" | 
+             RUELLE_CODE == "RV-VSMPE-10" | 
+             RUELLE_CODE == "RV-VSMPE-30" | 
+             RUELLE_CODE == "RV-VSMPE-22" | 
+             RUELLE_CODE == "RV-VSMPE-39" | 
+             RUELLE_CODE == "RV-VSMPE-29" |
+             RUELLE_CODE == "RV-VSMPE-35" |
+             RUELLE_CODE == "RV-VSMPE-26" |
+             RUELLE_CODE == "RV-VSMPE-31" |
+             RUELLE_CODE == "RV-VSMPE-36" |
+             RUELLE_CODE == "RV-VSMPE-25" |
+             RUELLE_CODE == "RV-VSMPE-23" |
+             RUELLE_CODE == "RV-VSMPE-32" |
+             RUELLE_CODE == "RV-VSMPE-40" |
+             RUELLE_CODE == "RV-VSMPE-13" |
+             RUELLE_CODE == "RV-VSMPE-16" |
+             RUELLE_CODE == "RV-VSMPE-17" |
+             RUELLE_CODE == "RV-VSMPE-20" |
+             RUELLE_CODE == "RV-VSMPE-21" |
+             RUELLE_CODE == "CON-VSMPE-1" |
+             RUELLE_CODE == "CON-VSMPE-7" |
+             RUELLE_CODE == "CON-VSMPE-8" |
+             RUELLE_CODE == "CON-VSMPE-10" |
+             RUELLE_CODE == "CON-VSMPE-4" )
   
   # Data separation ---------------------------------------------------------
   mont_rv <- study_rv %>% 
