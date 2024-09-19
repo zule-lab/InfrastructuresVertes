85b3af65749164848a8a611e174e3617ea4a9041
icrichmond <bellarichmond@proton.me>
2024-09-19 14:57:49 -0400

    ecological benefits models 1st draft

--- _targets.R
+++ _targets.R
@@ -110,19 +110,211 @@ targets_analysis <- c(
     cores = 4
   ),
   
+  zar_brms(
+    fireflies_vsmpe,
+    formula = firefly_presence ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = bernoulli(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 1.5), class = "Intercept"),
+      prior(normal(0, 0.2), class = "sd")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    sr_vsmpe,
+    formula = nSpecies ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = poisson(),
+    prior = c( 
+      prior(normal(0, 0.2), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(normal(0, 0.1), class = "sd")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    sr_tr,
+    formula = nSpecies ~ 1 + type,
+    family = poisson(),
+    prior = c( 
+      prior(normal(0, 0.2), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Trois-Rivières"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    fg_vsmpe,
+    formula = nFG ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = poisson(),
+    prior = c( 
+      prior(normal(0, 0.2), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(normal(0, 0.1), class = "sd")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    fg_tr,
+    formula = nFG ~ 1 + type,
+    family = poisson(),
+    prior = c( 
+      prior(normal(0, 0.2), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Trois-Rivières"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    vc_vsmpe,
+    formula = avg_complexity_s ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = gaussian(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(normal(0, 0.2), class = "sd"),
+      prior(exponential(1), class = "sigma")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    vc_tr,
+    formula = avg_complexity_s ~ 1 + type,
+    family = gaussian(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Trois-Rivières"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    pn_vsmpe,
+    formula = percent_nat ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = zero_one_inflated_beta(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(normal(0, 0.2), class = "sd"),
+      prior(gamma(0.01, 0.01), class = "phi"),
+      prior(beta(1, 1), class = "zoi"),
+      prior(beta(1, 1), class = "coi")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    pn_tr,
+    formula = percent_nat ~ 1 + type,
+    family = zero_one_inflated_beta(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(gamma(0.01, 0.01), class = "phi"),
+      prior(beta(1, 1), class = "zoi"),
+      prior(beta(1, 1), class = "coi")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Trois-Rivières"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  
+  zar_brms(
+    pi_vsmpe,
+    formula = percent_inv ~ 1 + type + per_fr_s + per_en_s + per_no_fren_s + medinc_s + (1 | Q_socio),
+    family = zero_one_inflated_beta(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(normal(0, 0.2), class = "sd"),
+      prior(gamma(0.01, 0.01), class = "phi"),
+      prior(beta(1, 1), class = "zoi"),
+      prior(beta(1, 1), class = "coi")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Villeray-Saint Michel-Parc Extension"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
+  zar_brms(
+    pi_tr,
+    formula = percent_inv ~ 1 + type,
+    family = zero_one_inflated_beta(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(gamma(0.01, 0.01), class = "phi"),
+      prior(beta(1, 1), class = "zoi"),
+      prior(beta(1, 1), class = "coi")
+    ),
+    backend = 'cmdstanr',
+    data = ecological_benefits %>% filter(city == "Trois-Rivières"),
+    chains = 4,
+    iter = 1000,
+    cores = 4
+  ),
+  
   # prior list
   tar_target(
     prior_model_list,
-    list(canopy_vsmpe_brms_sample_prior, canopy_tr_brms_sample_prior) %>%
-      setNames(., c('canopy_vsmpe_prior', 'canopy_tr_prior'))
+    list(canopy_vsmpe_brms_sample_prior, canopy_tr_brms_sample_prior, fireflies_vsmpe_brms_sample_prior, sr_vsmpe_brms_sample_prior, 
+         sr_tr_brms_sample_prior, fg_vsmpe_brms_sample_prior, fg_tr_brms_sample_prior, vc_vsmpe_brms_sample_prior, vc_tr_brms_sample_prior,
+          pn_vsmpe_brms_sample_prior, pn_tr_brms_sample_prior, pi_vsmpe_brms_sample_prior, pi_tr_brms_sample_prior) %>%
+      setNames(., c('canopy_vsmpe_prior', 'canopy_tr_prior', 'fireflies_vsmpe_prior', 'sr_vsmpe_prior', 'sr_tr_prior',
+                    'fg_vsmpe_prior', 'fg_tr_prior', 'vc_vsmpe_prior', 'vc_tr_prior', 'pn_vsmpe_prior', 'pn_tr_prior',
+                    'pi_vsmpe_prior', 'pi_tr_prior'))
     
   ),
   
   # model list
   tar_target(
     model_list,
-    list(canopy_vsmpe_brms_sample, canopy_tr_brms_sample) %>%
-      setNames(., c('canopy_vsmpe', 'canopy_tr'))
+    list(canopy_vsmpe_brms_sample, canopy_tr_brms_sample, fireflies_vsmpe_brms_sample, sr_vsmpe_brms_sample, sr_tr_brms_sample,
+         fg_vsmpe_brms_sample, fg_tr_brms_sample, vc_vsmpe_brms_sample, vc_tr_brms_sample, pn_vsmpe_brms_sample, pn_tr_brms_sample,
+         pi_vsmpe_brms_sample, pi_tr_brms_sample) %>%
+      setNames(., c('canopy_vsmpe', 'canopy_tr', 'fireflies_vsmpe', 'sr_vsmpe', 'sr_tr', 'fg_vsmpe', 'fg_tr',
+                    'vc_vsmpe', 'vc_tr', 'pn_vsmpe', 'pn_tr', 'pi_vsmpe', 'pi_tr'))
   ),
   
   
