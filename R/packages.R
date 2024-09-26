f9b85895d312e70f70e3e1980543500dff4b1b70
icrichmond <bellarichmond@proton.me>
2024-09-26 14:34:26 -0400

    suppress messages

--- R/packages.R
+++ R/packages.R
@@ -1,35 +1,37 @@
-library(targets)
-library(tarchetypes)
-library(qs)
-library(renv)
-
-library(sf)
-library(stars)
-
-library(readr)
-library(dplyr)
-library(stringr)
-library(tidyr)
-library(tibble)
-library(lubridate)
-
-library(suncalc)
-
-library(vegan)
-
-library(units)
-
-library(ggplot2)
-library(ggdag)
-library(ggrepel)
-library(cowplot)
-library(patchwork)
-library(gridExtra)
-
-library(osmdata)
-
-library(zarg)
-library(brms)
-library(cmdstanr)
-library(tidybayes)
-library(bayesplot)
+suppressPackageStartupMessages({
+  library(targets)
+  library(tarchetypes)
+  library(qs)
+  library(renv)
+  
+  library(sf)
+  library(stars)
+  
+  library(readr)
+  library(dplyr)
+  library(stringr)
+  library(tidyr)
+  library(tibble)
+  library(lubridate)
+  
+  library(suncalc)
+  
+  library(vegan)
+  
+  library(units)
+  
+  library(ggplot2)
+  library(ggdag)
+  library(ggrepel)
+  library(cowplot)
+  library(patchwork)
+  library(gridExtra)
+  
+  library(osmdata)
+  
+  library(zarg)
+  library(brms)
+  library(cmdstanr)
+  library(tidybayes)
+  library(bayesplot)
+  })
