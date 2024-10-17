fa005e33663dcd3aa14760f15cab5a63a6763768
icrichmond <bellarichmond@proton.me>
2024-10-17 12:08:18 -0400

    add TR plot

--- R/plot_temp_17h.R
+++ R/plot_temp_17h.R
@@ -25,11 +25,44 @@ plot_temp_17h <- function(cooling){
     geom_line(data = df[df$type == "Ruelle Verte",], aes(color = "Ruelle Verte"), alpha = 0.2) +
     geom_line(data = df[df$type == "Ruelle Traditionelle",], aes(color = "Moyenne Ruelle\nTraditionelle")) +
     scale_colour_manual(values = c("red", "grey20")) +
-    labs(x = "", colour = "", y = "Température à 17h00 (\u00B0C)") +
+    labs(x = "", colour = "", y = "Température à 17h00 (\u00B0C)", subtitle = "a) Villeray-Saint Michel-Parc Extension") +
     theme_classic() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
   
-  ggsave('graphics/temp_17h.png', p, width = 13, height = 8, units = 'in')
+  tr <- cooling %>%
+    filter(str_detect(plot_id, 'TR') == T) %>%
+    separate(date_time, c("date", "time"), sep = " ") %>%
+    filter(time == '17:00:00') %>%
+    mutate(type = case_when(str_detect(plot_id, "CON") == T ~ 'Ruelle grise',
+                            str_detect(plot_id, "RV") == T ~ 'Ruelle verte')) %>%
+    select(c(date, type, plot_id, temp_C))
+  
+  con_tr <- cooling %>%
+    filter(str_detect(plot_id, 'TR') == T) %>%
+    separate(date_time, c("date", "time"), sep = " ") %>%
+    filter(time == '17:00:00') %>%
+    group_by(date) %>%
+    distinct(con_id, .keep_all = T) %>%
+    summarize(temp_C = mean(temp_C_con),
+              type ='Ruelle grise',
+              plot_id = 'control_group')
+  
+  df_tr <- rbind(con_tr, tr)
+  
+  
+  p2 <- ggplot(df_tr, aes(date, temp_C, group = plot_id)) +
+    geom_line(data = df[df$type == "Ruelle verte",], aes(color = "Ruelle verte"), alpha = 0.2) +
+    geom_line(data = df[df$type == "Ruelle grise",], aes(color = "Moyenne ruelle\n grise")) +
+    scale_colour_manual(values = c("red", "grey20")) +
+    labs(x = "", colour = "", y = "Température à 17h00 (\u00B0C)", subtitle = "b) Trois-Rivières") +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  
+  
+  pt <- p / p2 
+  
+  ggsave('graphics/temp_17h.png', pt, width = 13, height = 13, units = 'in')
+  
   
 }
