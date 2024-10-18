6c87afdf45f20c19658c1e0b6bc15199885ee39b
icrichmond <bellarichmond@proton.me>
2024-10-18 09:23:56 -0400

    proportion + ruelle grise

--- R/plot_pa_firefly.R
+++ R/plot_pa_firefly.R
@@ -5,19 +5,25 @@ plot_pa_firefly <- function(fireflies_raw){
   presencecnt <- fireflies_raw %>% 
     group_by(Infrastructure.ID, Presence.Absence) %>% 
     tally() %>% 
-    mutate(type = case_when(str_detect(Infrastructure.ID, 'RV') == T ~ 'Ruelles Vertes',
-                            str_detect(Infrastructure.ID, 'SS') == T ~ 'Segments des Rues',
-                            str_detect(Infrastructure.ID, 'CON') == T ~ 'Ruelles Traditionelles')) %>%
+    mutate(type = case_when(str_detect(Infrastructure.ID, 'RV') == T ~ 'Ruelles vertes',
+                            str_detect(Infrastructure.ID, 'SS') == T ~ 'Segments des rues',
+                            str_detect(Infrastructure.ID, 'CON') == T ~ 'Ruelles grises')) %>%
     filter(Presence.Absence != "Absence") %>% 
     group_by(type) %>% 
-    tally()
+    tally() %>% 
+    mutate(n = case_when(type == 'Ruelles vertes' ~ (n/22)*100,
+                         type == 'Ruelles grises' ~ (n/5)*100,
+                         type == 'Segments des rues' ~ (n/27)*100 ))
+  
+  positions <- c('Ruelles vertes', 'Ruelles grises', 'Segments des rues')
   
   plot <- ggplot(presencecnt) + 
     geom_col(aes(x = type, y = n)) + 
     theme_classic() + 
     theme(axis.title = element_text(size = 14),
           axis.text = element_text(size = 12, colour = "black")) +
-    labs(x = "", y = "Nombre de sites avec présence de lucioles", title = "5 ruelles traditionelles au total, 22 ruelles vertes au total, 27 segments des rues au total")
+    scale_x_discrete(limits = positions) + 
+    labs(x = "", y = "Pourcentage de sites avec présence de lucioles (%)", title = "Villeray-Saint Michel-Parc Extension")
   
   ggsave('graphics/fireflypresence.png', plot, width = 8, height = 8, units = 'in')
 
