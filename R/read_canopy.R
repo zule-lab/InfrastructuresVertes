e4b8c9598e936ee94bda66b227e4b98d8ec21e51
i_richmo <i_richmo@BIOL-E6F40C.concordia.ca>
2025-09-13 16:00:04 -0400

    read tif directly, links broken

--- R/read_canopy.R
+++ R/read_canopy.R
@@ -1,10 +1,12 @@
 read_canopy <- function(mtl_can, tr_can) {
   
 
-  mtl <- download_tif('https://cartes.inspq.qc.ca/diffusion/donnee_publique/canopee/31hl102.tif', 'input/mon-canopy.tif')
+  #mtl <- download_tif('https://cartes.inspq.qc.ca/diffusion/donnee_publique/canopee/31hl102.tif', 'input/mon-canopy.tif')
+  #tr <- download_tif('https://cartes.inspq.qc.ca/diffusion/donnee_publique/canopee/31ig102.tif', 'input/tr-canopy.tif')
   
-  tr <- download_tif('https://cartes.inspq.qc.ca/diffusion/donnee_publique/canopee/31ig102.tif', 'input/tr-canopy.tif')
+  mtl <- read_stars('input/mon-canopy.tif', proxy = TRUE)
   
+  tr <- read_stars('input/tr-canopy.tif', proxy = TRUE)
   
   # Save mosaic as file in output/
   can <- st_mosaic(mtl, tr, dst = file.path('output', 'canopy_mosaic.tif'))
